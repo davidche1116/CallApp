853cd8eea47d6b09560215d452ed27853e0de649
cheyang <cheyang@cecdat.com>
2024-11-14 10:30:23 +0800

    image edit

--- lib/page/image_edit_page.dart
+++ lib/page/image_edit_page.dart
@@ -1,11 +1,15 @@
+import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
 
-import 'package:call/utls/widget_util.dart';
 import 'package:extended_image/extended_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
-import 'package:image_editor/image_editor.dart' hide ImageSource;
+import 'package:flutter_ruler_picker/flutter_ruler_picker.dart';
+
+import '../utls/crop_editor_helper.dart';
+import '../utls/widget_util.dart';
 
 class ImageEditPage extends StatefulWidget {
   final File _file;
@@ -17,163 +21,232 @@ class ImageEditPage extends StatefulWidget {
 }
 
 class _ImageEditPageState extends State<ImageEditPage> {
-  final GlobalKey<ExtendedImageEditorState> _editorKey = GlobalKey();
-
-  late ImageProvider _provider;
+  final ImageEditorController _editorController = ImageEditorController();
+  final MyRulerPickerController _rulerPickerController =
+      MyRulerPickerController(value: 0.0);
 
   @override
   void initState() {
     super.initState();
-
-    _provider = ExtendedFileImageProvider(
-      widget._file,
-      cacheRawData: true,
-    );
   }
 
   @override
   Widget build(BuildContext context) {
+    ImageProvider imageProvider = ExtendedFileImageProvider(
+      widget._file,
+      cacheRawData: true,
+    );
+
     return Scaffold(
-      body: SafeArea(
-        child: Stack(
-          children: [
-            _buildImage(),
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  _buildBack(),
-                  const Spacer(),
-                  _buildButton(),
-                ],
-              ),
+      body: Stack(
+        children: [
+          ExtendedImage(
+            image: imageProvider,
+            fit: BoxFit.contain,
+            mode: ExtendedImageMode.editor,
+            enableLoadState: true,
+            initEditorConfigHandler: (ExtendedImageState? state) {
+              return EditorConfig(
+                maxScale: 16.0,
+                cropRectPadding: const EdgeInsets.all(20.0),
+                hitTestSize: 20.0,
+                cropLayerPainter: const EditorCropLayerPainter(),
+                initCropRectType: InitCropRectType.imageRect,
+                cropAspectRatio: CropAspectRatios.ratio1_1,
+                controller: _editorController,
+                cornerColor: Theme.of(context).colorScheme.onPrimary,
+              );
+            },
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                AppBar(
+                  backgroundColor: Colors.transparent,
+                  actions: [
+                    FilledButton(
+                      onPressed: () async {
+                        WidgetUtil.showLoading('处理中');
+
+                        /// 裁剪
+                        var list = await _cropImage(true);
+
+                        /// 压缩
+                        var result =
+                            await FlutterImageCompress.compressWithList(
+                          list!,
+                          minHeight: 800,
+                          minWidth: 800,
+                          quality: 90,
+                        );
+
+                        WidgetUtil.hideLoading();
+
+                        if (context.mounted) {
+                          Navigator.pop(context, result);
+                        }
+                      },
+                      child: const Text(
+                        '完成',
+                      ),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: LayoutBuilder(
+                          builder: (BuildContext c, BoxConstraints b) {
+                            return RulerPicker(
+                              controller: _rulerPickerController,
+                              rulerBackgroundColor: Colors.transparent,
+                              onValueChanged: (num value) {
+                                if (_rulerPickerController.value
+                                        .toDouble()
+                                        .equalTo(value.toDouble()) &&
+                                    !_onUndoOrRedoing) {
+                                  return;
+                                }
+                                HapticFeedback.vibrate();
+
+                                _editorController.rotate(
+                                  degree: value.toDouble() -
+                                      _rulerPickerController.value,
+                                );
+
+                                _rulerPickerController
+                                    .setValueWithOutNotify(value);
+                              },
+                              width: b.maxWidth,
+                              height: 50,
+                              onBuildRulerScaleText:
+                                  (int index, num rulerScaleValue) {
+                                return '$rulerScaleValue';
+                              },
+                              ranges: const <RulerRange>[
+                                RulerRange(begin: -45, end: 45, scale: 1),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _onUndoOrRedo(() {
+                            _editorController.undo();
+                          });
+                        },
+                        icon: Icon(
+                          Icons.undo,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _onUndoOrRedo(() {
+                            _editorController.redo();
+                          });
+                        },
+                        icon: Icon(
+                          Icons.redo,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _rulerPickerController.value = 0;
+                          _editorController.reset();
+                        },
+                        icon: Icon(
+                          Icons.restore,
+                          size: 30,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _editorController.flip(
+                            animation: true,
+                          );
+                        },
+                        icon: const Icon(Icons.flip, size: 30),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          _editorController.rotate(
+                            degree: 90,
+                            animation: true,
+                            rotateCropRect: true,
+                          );
+                        },
+                        icon: const Icon(Icons.rotate_right, size: 30),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildBack() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-            size: 30,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildButton() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        IconButton(
-          onPressed: () => _flip(),
-          icon: const Icon(Icons.flip, size: 30),
-        ),
-        IconButton(
-          onPressed: () => _rotate(false),
-          icon: const Icon(Icons.rotate_left, size: 30),
-        ),
-        IconButton(
-          onPressed: () => _rotate(true),
-          icon: const Icon(Icons.rotate_right, size: 30),
-        ),
-        FilledButton(
-          onPressed: () async {
-            WidgetUtil.showLoading('处理中');
-
-            /// 裁剪
-            var list = await _crop();
-
-            /// 压缩
-            var result = await FlutterImageCompress.compressWithList(
-              list!,
-              minHeight: 800,
-              minWidth: 800,
-              quality: 90,
-            );
-
-            WidgetUtil.hideLoading();
-            if (!mounted) return;
-            Navigator.pop(context, result);
-          },
-          child: const Text(
-            '完成',
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildImage() {
-    return ExtendedImage(
-      image: _provider,
-      extendedImageEditorKey: _editorKey,
-      mode: ExtendedImageMode.editor,
-      fit: BoxFit.contain,
-      initEditorConfigHandler: (_) => EditorConfig(
-          maxScale: 16.0,
-          cropRectPadding: const EdgeInsets.all(20.0),
-          hitTestSize: 20.0,
-          cropAspectRatio: CropAspectRatios.ratio1_1,
-          cornerColor:
-              Theme.of(context).colorScheme.onPrimary.withValues(alpha: 0.7)),
-    );
-  }
-
-  Future<Uint8List?> _crop() async {
-    final ExtendedImageEditorState? state = _editorKey.currentState;
-    if (state == null) {
-      return null;
+  bool _onUndoOrRedoing = false;
+  void _onUndoOrRedo(Function fn) {
+    final double oldRotateDegrees = _editorController.rotateDegrees;
+    _onUndoOrRedoing = true;
+    fn();
+    _onUndoOrRedoing = false;
+    final double newRotateDegrees = _editorController.rotateDegrees;
+    if (oldRotateDegrees != newRotateDegrees &&
+        !(newRotateDegrees - oldRotateDegrees).isZero &&
+        (newRotateDegrees - oldRotateDegrees) % 90 != 0) {
+      _rulerPickerController.value =
+          _rulerPickerController.value + (newRotateDegrees - oldRotateDegrees);
     }
-    final Rect? rect = state.getCropRect();
-    if (rect == null) {
-      return null;
-    }
-    final EditActionDetails action = state.editAction!;
-    final double radian = action.rotateAngle;
+  }
 
-    final bool flipHorizontal = action.flipY;
-    final bool flipVertical = action.flipX;
-    final Uint8List img = state.rawImageData;
-
-    if (img.isEmpty) {
-      return null;
+  Future<Uint8List?> _cropImage(bool useNative) async {
+    String msg = '';
+    try {
+      EditImageInfo imageInfo =
+          await cropImageDataWithNativeLibrary(_editorController);
+      return imageInfo.data;
+    } catch (e, stack) {
+      msg = 'save failed: $e\n $stack';
+      debugPrint(msg);
     }
 
-    final ImageEditorOption option = ImageEditorOption();
+    return null;
+  }
+}
 
-    option.addOption(ClipOption.fromRect(rect));
-    option.addOption(
-        FlipOption(horizontal: flipHorizontal, vertical: flipVertical));
-    if (action.hasRotateAngle) {
-      option.addOption(RotateOption(radian.toInt()));
+class MyRulerPickerController extends RulerPickerController {
+  MyRulerPickerController({num value = 0}) : _value = value;
+  @override
+  num get value => _value;
+  num _value;
+  @override
+  set value(num newValue) {
+    if (_value == newValue) {
+      return;
     }
-
-    option.outputFormat = const OutputFormat.jpeg(80);
-
-    final Uint8List? result = await ImageEditor.editImage(
-      image: img,
-      imageEditorOption: option,
-    );
-
-    return result;
+    _value = newValue;
+    notifyListeners();
   }
 
-  void _flip() {
-    _editorKey.currentState?.flip();
-  }
-
-  void _rotate(bool right) {
-    _editorKey.currentState?.rotate(right: right);
+  void setValueWithOutNotify(num newValue) {
+    _value = newValue;
   }
 }
---
+++ lib/utls/crop_editor_helper.dart
@@ -0,0 +1,65 @@
+import 'dart:ui';
+
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/foundation.dart';
+import 'package:image_editor/image_editor.dart';
+
+enum ImageType { gif, jpg }
+
+class EditImageInfo {
+  EditImageInfo(
+    this.data,
+    this.imageType,
+  );
+  final Uint8List? data;
+  final ImageType imageType;
+}
+
+Future<EditImageInfo> cropImageDataWithNativeLibrary(
+    ImageEditorController imageEditorController) async {
+  debugPrint('native library start cropping');
+
+  final EditActionDetails action = imageEditorController.editActionDetails!;
+
+  final Uint8List img = imageEditorController.state!.rawImageData;
+
+  final ImageEditorOption option = ImageEditorOption();
+
+  if (action.hasRotateDegrees) {
+    final int rotateDegrees = action.rotateDegrees.toInt();
+    option.addOption(RotateOption(rotateDegrees));
+  }
+  if (action.flipY) {
+    option.addOption(const FlipOption(horizontal: true, vertical: false));
+  }
+
+  if (action.needCrop) {
+    Rect cropRect = imageEditorController.getCropRect()!;
+    if (imageEditorController.state!.widget.extendedImageState.imageProvider
+        is ExtendedResizeImage) {
+      final ImmutableBuffer buffer = await ImmutableBuffer.fromUint8List(img);
+      final ImageDescriptor descriptor = await ImageDescriptor.encoded(buffer);
+
+      final double widthRatio =
+          descriptor.width / imageEditorController.state!.image!.width;
+      final double heightRatio =
+          descriptor.height / imageEditorController.state!.image!.height;
+      cropRect = Rect.fromLTRB(
+        cropRect.left * widthRatio,
+        cropRect.top * heightRatio,
+        cropRect.right * widthRatio,
+        cropRect.bottom * heightRatio,
+      );
+    }
+    option.addOption(ClipOption.fromRect(cropRect));
+  }
+
+  final DateTime start = DateTime.now();
+  final Uint8List? result = await ImageEditor.editImage(
+    image: img,
+    imageEditorOption: option,
+  );
+
+  debugPrint('${DateTime.now().difference(start)} ：total time');
+  return EditImageInfo(result, ImageType.jpg);
+}
